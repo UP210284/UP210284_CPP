babe925445517ca4aeb91ee0b8a4c123be4b7db0
UP210284 <up210284@alumnos.upa.edu.mx>
2022-09-20 13:19:56 -0500

    carga del 20/09

---
+++ U1/Ciclos.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    int suma = 0, par=0, impar=0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        
+            printf("Contador %d \n", i);
+        
+        suma = suma + i;
+        if(i%2==0)
+        {
+            par=par+i;
+        }
+        else
+        {
+            impar=impar+i;
+        }
+    }
+    printf("La suma de %d numeros es de: %d\n", n, suma);
+    printf("La suma de los pares es: %d\n", par);
+    printf("La suma de los impares es: %d\n", impar);
+
+    return 0;
+}
